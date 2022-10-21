2fbed25c4ad10d6310f8b41b2ebb797afed7fbda
John Doe <marvin.bolanos25@gmail.com>
2022-10-21 09:10:35 -0700

    reservation for both single and multi parts added

--- components/muiComponents/Counter.js
+++ components/muiComponents/Counter.js
@@ -1,228 +1,138 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Tooltip from '@mui/material/Tooltip';
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
-import {
-  addDoc,
-  collection,
-  serverTimestamp,
-  getDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  onSnapshot,
-  query,
-  orderBy,
-  collectionGroup,
-  getDocs,
-  where,
-} from '@firebase/firestore';
+import { doc, updateDoc } from '@firebase/firestore';
 import { db } from '../../config/firebase';
+import * as partService from '../../services/addPartServices';
 
-const App = ({
-  reservedValue,
-  shelfValue,
-  modelId,
-  partId,
-  shared,
-  partNumber,
-  modelNameShared,
-}) => {
-  let model1 = [];
-  let model2 = [];
-
-  const sharedDocRef = query(
-    collection(db, 'Shared Part Numbers'),
-    where('partNumber', '==', partNumber)
-  );
-
-  const docRef = doc(db, 'iPhone Models', modelId, 'Parts', partId);
+const Counter = ({ modelId, partId, partData }) => {
+  const singleModelDocRef = doc(db, 'iPhone Models', modelId, 'Parts', partId);
+  const docRefModel1 = null;
+  const docRefModel2 = null;
+  const sharedPartsData = null;
 
   const IncNum = async () => {
-    if (shelfValue == 0) {
-    } else if (shelfValue > reservedValue && shared.toString() == 'true') {
-      const q2 = query(
-        collection(db, 'iPhone Models'),
-        where('model', '==', modelNameShared[0])
-      );
+    if (partData.stock == 0) {
+    } else if (
+      partData.stock > partData.reserved &&
+      partData.sharedPartNumber == true
+    ) {
+      sharedPartsData = await partService.CheckPartNumber(partData.partNumber);
 
-      const queryModel2Snapshot = await getDocs(q2);
-      queryModel2Snapshot.forEach((doc) => {
-        model1.push({ id: doc.id, ...doc.data() });
-      });
+      if (sharedPartsData.length == 2) {
+        docRefModel1 = doc(
+          db,
+          'iPhone Models',
+          sharedPartsData[0].modelId,
+          'Parts',
+          sharedPartsData[0].id
+        );
 
-      const q3 = query(
-        collection(db, 'iPhone Models'),
-        where('model', '==', modelNameShared[1])
-      );
-
-      const queryModel3Snapshot = await getDocs(q3);
-      queryModel3Snapshot.forEach((doc) => {
-        model2.push({ id: doc.id, ...doc.data() });
-      });
-
-      const serialQuery = query(
-        collectionGroup(db, 'Parts'),
-        where('partNumber', '==', partNumber)
-      );
-      const queryPartNumber = await getDocs(serialQuery);
-
-      const partsData = queryPartNumber.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      const docRefModel1 = doc(
-        db,
-        'iPhone Models',
-        model1[0].id,
-        'Parts',
-        partsData[0].id
-      );
-      const docRefModel2 = doc(
-        db,
-        'iPhone Models',
-        model2[0].id,
-        'Parts',
-        partsData[1].id
-      );
-
-      updateDoc(docRefModel1, {
-        reserved: reservedValue + 1,
-      });
-      updateDoc(docRefModel2, {
-        reserved: reservedValue + 1,
-      });
-    } else if (shelfValue > reservedValue) {
-      updateDoc(docRef, {
-        reserved: reservedValue + 1,
+        docRefModel2 = doc(
+          db,
+          'iPhone Models',
+          sharedPartsData[1].modelId,
+          'Parts',
+          sharedPartsData[1].id
+        );
+        updateDoc(docRefModel1, {
+          reserved: partData.reserved + 1,
+          available: partData.available - 1,
+        });
+        updateDoc(docRefModel2, {
+          reserved: partData.reserved + 1,
+          available: partData.available - 1,
+        });
+      }
+    } else if (partData.stock > partData.reserved) {
+      updateDoc(singleModelDocRef, {
+        reserved: partData.reserved + 1,
+        available: partData.available - 1,
       });
     }
   };
 
+  //error when decreasing
   const DecNum = async () => {
     const docRef2 = doc(db, 'iPhone Models', modelId, 'Parts', partId);
 
-    if (shelfValue == 0) {
-    } else if (shelfValue == reservedValue) {
-      if (shared.toString() == 'true') {
-        const q2 = query(
-          collection(db, 'iPhone Models'),
-          where('model', '==', modelNameShared[0])
+    if (partData.stock == 0) {
+    } else if (partData.stock == partData.reserved) {
+      if (partData.sharedPartNumber == true) {
+        sharedPartsData = await partService.CheckPartNumber(
+          partData.partNumber
         );
 
-        const queryModel2Snapshot = await getDocs(q2);
-        queryModel2Snapshot.forEach((doc) => {
-          model1.push({ id: doc.id, ...doc.data() });
-        });
+        if (sharedPartsData.length == 2) {
+          docRefModel1 = doc(
+            db,
+            'iPhone Models',
+            sharedPartsData[0].modelId,
+            'Parts',
+            sharedPartsData[0].id
+          );
 
-        const q3 = query(
-          collection(db, 'iPhone Models'),
-          where('model', '==', modelNameShared[1])
-        );
-
-        const queryModel3Snapshot = await getDocs(q3);
-        queryModel3Snapshot.forEach((doc) => {
-          model2.push({ id: doc.id, ...doc.data() });
-        });
-
-        const serialQuery = query(
-          collectionGroup(db, 'Parts'),
-          where('partNumber', '==', partNumber)
-        );
-        const queryPartNumber = await getDocs(serialQuery);
-
-        const partsData = queryPartNumber.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-
-        const docRefModel1 = doc(
-          db,
-          'iPhone Models',
-          model1[0].id,
-          'Parts',
-          partsData[0].id
-        );
-        const docRefModel2 = doc(
-          db,
-          'iPhone Models',
-          model2[0].id,
-          'Parts',
-          partsData[1].id
-        );
-
-        updateDoc(docRefModel1, {
-          reserved: reservedValue - 1,
-        });
-        updateDoc(docRefModel2, {
-          reserved: reservedValue - 1,
-        });
+          docRefModel2 = doc(
+            db,
+            'iPhone Models',
+            sharedPartsData[1].modelId,
+            'Parts',
+            sharedPartsData[1].id
+          );
+          updateDoc(docRefModel1, {
+            reserved: partData.reserved - 1,
+            available: partData.available + 1,
+          });
+          updateDoc(docRefModel2, {
+            reserved: partData.reserved - 1,
+            available: partData.available + 1,
+          });
+        }
       } else {
         updateDoc(docRef2, {
-          reserved: reservedValue - 1,
+          reserved: partData.reserved - 1,
+          available: partData.available + 1,
         });
       }
-    } else if (shelfValue > reservedValue) {
-      if (reservedValue == 0) {
-      } else if (shared.toString() == 'true') {
-        const q2 = query(
-          collection(db, 'iPhone Models'),
-          where('model', '==', modelNameShared[0])
+    } else if (partData.stock > partData.reserved) {
+      if (partData.reserved == 0) {
+      } else if (partData.sharedPartNumber == true) {
+        sharedPartsData = await partService.CheckPartNumber(
+          partData.partNumber
         );
 
-        const queryModel2Snapshot = await getDocs(q2);
-        queryModel2Snapshot.forEach((doc) => {
-          model1.push({ id: doc.id, ...doc.data() });
-        });
+        if (sharedPartsData.length == 2) {
+          docRefModel1 = doc(
+            db,
+            'iPhone Models',
+            sharedPartsData[0].modelId,
+            'Parts',
+            sharedPartsData[0].id
+          );
 
-        const q3 = query(
-          collection(db, 'iPhone Models'),
-          where('model', '==', modelNameShared[1])
-        );
-
-        const queryModel3Snapshot = await getDocs(q3);
-        queryModel3Snapshot.forEach((doc) => {
-          model2.push({ id: doc.id, ...doc.data() });
-        });
-
-        const serialQuery = query(
-          collectionGroup(db, 'Parts'),
-          where('partNumber', '==', partNumber)
-        );
-        const queryPartNumber = await getDocs(serialQuery);
-
-        const partsData = queryPartNumber.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-
-        const docRefModel1 = doc(
-          db,
-          'iPhone Models',
-          model1[0].id,
-          'Parts',
-          partsData[0].id
-        );
-        const docRefModel2 = doc(
-          db,
-          'iPhone Models',
-          model2[0].id,
-          'Parts',
-          partsData[1].id
-        );
-
-        updateDoc(docRefModel1, {
-          reserved: reservedValue - 1,
-        });
-        updateDoc(docRefModel2, {
-          reserved: reservedValue - 1,
-        });
+          docRefModel2 = doc(
+            db,
+            'iPhone Models',
+            sharedPartsData[1].modelId,
+            'Parts',
+            sharedPartsData[1].id
+          );
+          updateDoc(docRefModel1, {
+            reserved: partData.reserved - 1,
+            available: partData.available + 1,
+          });
+          updateDoc(docRefModel2, {
+            reserved: partData.reserved - 1,
+            available: partData.available + 1,
+          });
+        }
       } else {
         updateDoc(docRef2, {
-          reserved: reservedValue - 1,
+          reserved: partData.reserved - 1,
+          available: partData.available + 1,
         });
       }
     }
@@ -242,7 +152,7 @@ const App = ({
         </Button>
       </Tooltip>
       <Box sx={{ marginLeft: '10px', marginRight: '10px' }}>
-        {reservedValue}
+        {partData.reserved}
       </Box>
 
       <Tooltip title="Subtract">
@@ -253,4 +163,4 @@ const App = ({
     </Box>
   );
 };
-export default App;
+export default Counter;
